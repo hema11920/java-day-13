88cd25206458f628abd103b70deb2fcea90f04b1
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-14 15:02:16 +0530

    Add files via upload

---
+++ throwkeywordinexception.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class throwkeywordinexception{
+    public static void main(String args []){
+        //try-catch
+        try{
+            throw new Exception("Error");
+        }catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
